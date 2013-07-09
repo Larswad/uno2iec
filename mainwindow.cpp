cb825ccfaaa7d4808454762ecfb2ede13df64317
Lars Wadefalk <lars@wadefalk.se>
2013-07-09 23:35:46 +0200

    changed check for whether wiringPi initialization fails. Always trying to export pins.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -50,10 +50,9 @@ MainWindow::MainWindow(QWidget *parent) :
 
 	connect(&m_port, SIGNAL(readyRead()), this, SLOT(onDataAvailable()));
 #ifdef HAS_WIRINGPI
-	if(!wiringPiSetupSys())
+	if(-1 == wiringPiSetupSys())
 		Log("MAIN", "Failed initializing WiringPi. Continuing anyway...", error);
-	else
-		system("/usr/local/bin/gpio export 23 out");
+	system("/usr/local/bin/gpio export 23 out");
 #endif
 	Log("MAIN", "Application Initialized.", success);
 } // MainWindow
