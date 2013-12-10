cc468020b28d069c267a836ea37765663b876dbb
Lars Wadefalk <lars@wadefalk.se>
2013-12-10 01:04:41 +0100

    Wrote down version information. Increased version to 0.4.0.0. Ready for tagging.

--- version.h
+++ version.h
@@ -2,10 +2,10 @@
 #define VERSION_H
 
 #define VER_FILEVERSION							0,3,0,0
-#define VER_FILEVERSION_STR					"0.3.0.0\0"
+#define VER_FILEVERSION_STR					"0.4.0.0\0"
 
-#define VER_PRODUCTVERSION					0,3,0,0
-#define VER_PRODUCTVERSION_STR			"0.3.0\0"
+#define VER_PRODUCTVERSION					0,4,0,0
+#define VER_PRODUCTVERSION_STR			"0.4.0\0"
 
 #define VER_COMPANYNAME_STR					"MumboJumbo_Software"
 #define VER_FILEDESCRIPTION_STR			"uno2iec"
