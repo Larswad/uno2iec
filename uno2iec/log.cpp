81fac54793411c166f1d554336df84cc47f75914
Lars Wadefalk <lars@wadefalk.se>
2014-12-29 10:41:16 +0100

    uchar_prog definition obsoleted, changed to PROGMEM.

--- uno2iec/log.cpp
+++ uno2iec/log.cpp
@@ -4,8 +4,8 @@
 #ifndef NO_LOGGING
 
 const struct {
-	prog_uchar abbreviated;
-	prog_uchar string[6]; // <-- Set this to the longest one including \0
+	char PROGMEM abbreviated;
+	char PROGMEM string[6]; // <-- Set this to the longest one including \0
 } facilities[] PROGMEM = { FAC_MAIN, "MAIN", FAC_IEC, "IEC", FAC_IFACE, "IFACE" };
 
 static const char siwe[] PROGMEM = "SIWE";
